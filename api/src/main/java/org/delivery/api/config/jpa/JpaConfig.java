447949447b8a142336fe2e1895bedee02bce9ce3
seeeop2 <seeeop2@gmail.com>
2024-05-13 23:53:16 +0900

    리팩토링: 패키지 변경
    패키지 경로를 잘못 설정해서 수정함.

--- api/src/main/java/org/delivery/api/config/jpa/JpaConfig.java
+++ api/src/main/java/org/delivery/api/config/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package org.delivery.config;
+package org.delivery.api.config.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
