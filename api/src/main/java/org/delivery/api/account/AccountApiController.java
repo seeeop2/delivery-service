aaa03098be55def57377ebf1cded080240969897
seeeop2 <seeeop2@gmail.com>
2024-05-15 01:08:55 +0900

    Account 컨트롤러에 save 메소드 삭제 및 me 메소드 추가
    임시로 만들었던 save()를 삭제함.
    
    이전에 만들었던 AccountMeResponse 클래스로 반환을 하는 me() 추가함.
    임시로 데이터 넣어둠.

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,13 @@
-package org.delivery.account;
+package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
-import org.delivery.db.account.AccountEntity;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,10 +16,12 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save(){
-            AccountEntity account = AccountEntity.builder().build();
-            accountRepository.save(account);
-
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("A@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
