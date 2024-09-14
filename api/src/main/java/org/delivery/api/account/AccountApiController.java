88b7578e3d3779cd78093ad6e9d371d5fab6cf66
Seeeop2 <seeeop2@gmail.com>
2024-09-14 10:05:53 +0900

    REFACTOR: AccountApiController에서 Api 클래스 사용으로 응답 형식 변경
    - AccountApiController의 me() 메서드에서 응답 형식을 Api<AccountMeResponse>로 변경함.
    - AccountMeResponse 객체를 Api.OK() 메서드를 통해 반환하도록 수정함.
    - API 응답을 일관된 형식으로 관리할 수 있도록 개선함.

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -2,6 +2,7 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,15 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me(){
-        return AccountMeResponse.builder()
-                .name("홍길동")
-                .email("A@gmail.com")
-                .registeredAt(LocalDateTime.now())
-                .build();
+    public Api<AccountMeResponse> me(){
+
+        AccountMeResponse response =
+                AccountMeResponse.builder()
+                        .name("홍길동")
+                        .email("A@gmail.com")
+                        .registeredAt(LocalDateTime.now())
+                        .build();
+
+        return Api.OK(response);
     }
 }
