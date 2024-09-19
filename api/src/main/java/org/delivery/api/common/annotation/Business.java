092f887f477d859f30ea2003d1417a3f5350a48a
Seeeop2 <seeeop2@gmail.com>
2024-09-20 04:23:12 +0900

    FEAT: Business 및 Converter 사용자 정의 어노테이션 추가
    - 비즈니스 로직을 나타내는 Business 사용자 정의 어노테이션 추가.
    - 변환 로직을 나타내는 Converter 사용자 정의 어노테이션 추가.
    - 두 어노테이션 모두 클래스, 인터페이스, 열거형에 적용 가능하며, Spring의 @Service 어노테이션을 포함하여 자동으로 서비스로 등록됨.
    - @AliasFor를 사용하여 @Service의 value 속성을 재정의 가능함.

---
+++ api/src/main/java/org/delivery/api/common/annotation/Business.java
@@ -0,0 +1,25 @@
+package org.delivery.api.common.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 비즈니스 로직을 나타내는 사용자 정의 어노테이션
+
+// 이 어노테이션은 클래스, 인터페이스, 열거형에 적용 가능
+@Target(ElementType.TYPE)
+// 런타임 시에 어노테이션 정보가 유지됨
+@Retention(RetentionPolicy.RUNTIME)
+// Spring의 Service 어노테이션을 포함하여 자동으로 서비스로 등록됨
+@Service
+public @interface Business {
+
+    // Service 어노테이션의 value 속성을 재정의하기 위한 AliasFor
+    @AliasFor(annotation = Service.class)
+    String value() default "";
+
+}
