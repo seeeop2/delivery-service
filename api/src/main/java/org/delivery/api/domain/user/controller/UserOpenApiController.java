6542e10080f9f6477d4d856cd2fc9fccf2106b19
Seeeop2 <seeeop2@gmail.com>
2024-09-21 01:46:48 +0900

    FEAT: 인증 및 인가 없이 사용할 수 있는 공개된 API를 처리하는 UserOpenApiController 클래스 추가
    - 인증 및 인가 없이 사용할 수 있는 공개된 API를 처리하는 UserOpenApiController 클래스를 추가함.
    - 사용자 가입 요청을 처리하는 register 메서드를 구현함.
      - @Valid 어노테이션을 사용하여 요청 파라미터의 유효성 검사를 수행함.
      - 요청 본문에서 UserRegisterRequest를 추출하여 UserBusiness에 전달함.
      - 사용자 가입 처리 후 Api 형식으로 성공적인 응답을 반환함.
    - RESTful API 구조를 통해 인증 및 인가 없이 사용할 수 있는 사용자 가입 기능을 외부에 제공함.

---
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -0,0 +1,34 @@
+package org.delivery.api.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/open-api/user")
+@RestController
+public class UserOpenApiController {
+
+    private final UserBusiness userBusiness;
+
+    // 사용자 가입 요청 처리
+    // 파라미터에 @Valid 어노테이션을 사용하여 유효성 검사를 수행함
+    // 즉, 해당 파라미터의 클래스에 적용된 validation 을 모두 체크함.
+    @PostMapping("/register")
+    public Api<UserResponse> register(@Valid @RequestBody Api<UserRegisterRequest> request){
+
+        // 요청 본문에서 UserRegisterRequest를 추출하여 사용자 비즈니스 로직에 전달
+        UserResponse response = userBusiness.register(request.getBody());
+        // 성공적인 응답을 Api 형식으로 반환
+        return Api.OK(response);
+
+    }
+}
