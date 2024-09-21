301d41a636cbf01821c0dcadd6fca609ea489c1f
Seeeop2 <seeeop2@gmail.com>
2024-09-21 11:38:37 +0900

    REFACTOR: AccountApiController의 API 경로 변경
    - AccountApiController의 API 경로를 "/api/account"에서 "/example/api/account"로 변경함.

--- api/src/main/java/org/delivery/api/example/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/example/account/AccountApiController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/example/api/account")
 public class AccountApiController {
 
     private final AccountRepository accountRepository;
