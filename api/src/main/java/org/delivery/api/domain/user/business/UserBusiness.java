e6bef26922d62b0b01b16c5edbfffb9c966bc0a2
Seeeop2 <seeeop2@gmail.com>
2024-09-25 00:28:14 +0900

    FEAT: UserBusiness 클래스에 로그인 기능 수정 및 토큰 생성 추가
    - UserBusiness 클래스에 TokenBusiness를 주입하여 로그인 시 토큰 생성을 추가함.
    - login 메서드의 반환 타입을 UserResponse에서 TokenResponse로 변경하여, 로그인 후 토큰 정보를 반환하도록 수정함.
    - 사용자 확인 후 토큰을 생성하는 로직을 구현하여 사용자 인증 프로세스를 강화함.
    - 기존의 사용자 등록 로직은 변경 없이 유지하며, 코드의 가독성을 개선하기 위한 주석 추가함.

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -2,6 +2,8 @@ package org.delivery.api.domain.user.business;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
+import org.delivery.api.domain.token.business.TokenBusiness;
+import org.delivery.api.domain.token.controller.model.TokenResponse;
 import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
@@ -17,6 +19,7 @@ public class UserBusiness {
 
     private final UserService userService;
     private final UserConverter userConverter;
+    private final TokenBusiness tokenBusiness;
 
     // 사용자에 대한 가입 처리 로직
     // 1. request -> entity
@@ -48,13 +51,12 @@ public class UserBusiness {
     // 2. user entity 로그인 확인
     // 3. token 생성
     // 4. token response
-    public UserResponse login(UserLoginRequest request) {
+    public TokenResponse login(UserLoginRequest request) {
 
         UserEntity userEntity = userService.login(request.getEmail(), request.getPassword());
+        TokenResponse tokenResponse = tokenBusiness.issueToken(userEntity);
 
-        // 사용자 없으면 throw
-
-        // TODO 토큰 생성 로직으로 변경하기
-        return userConverter.toResponse(userEntity);
+        return tokenResponse;
     }
+
 }
