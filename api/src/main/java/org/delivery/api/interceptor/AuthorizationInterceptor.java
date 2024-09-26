dfd507ed504c9b446ad4194ec3bb359830b34f87
Seeeop2 <seeeop2@gmail.com>
2024-09-27 02:33:10 +0900

    FEAT: AuthorizationInterceptor에 JWT 토큰 검증 및 사용자 ID 설정 기능 추가
    - AuthorizationInterceptor 클래스에 JWT 토큰 검증 로직을 추가하여, 요청 헤더에서 "authorization-token"을 가져옴.
    - 토큰이 없는 경우 TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND 예외를 발생시킴.
    - 유효한 액세스 토큰에 대해 사용자 ID를 검증하고, 이를 현재 요청의 속성에 저장하는 기능을 구현함.
    - 인증 성공 시 요청 속성에 "userId"라는 키로 사용자 ID를 설정하고, 인증 실패 시 BAD_REQUEST 예외를 발생시킴.
    - 정적 리소스 요청 및 OPTIONS 메서드에 대해서는 인증 절차를 통과하도록 설정함.

--- api/src/main/java/org/delivery/api/interceptor/AuthorizationInterceptor.java
+++ api/src/main/java/org/delivery/api/interceptor/AuthorizationInterceptor.java
@@ -2,18 +2,27 @@ package org.delivery.api.interceptor;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.TokenErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.business.TokenBusiness;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    private final TokenBusiness tokenBusiness;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -31,10 +40,32 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        // TODO header 검증
+        // 우선 ModHeader 크롬 확장프로그램을 설치하여,
+        // 로그인 후 발급받은 JWT 토큰을 임의로 authorization-token 헤더명으로 등록
+        // 테스트 시, authorization-token 헤더 가져올 수 있음.
+        String accessToken = request.getHeader("authorization-token");
 
+        // 토큰이 없을 경우 예외 발생
+        if (accessToken == null) {
+            throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
+        }
 
-        // 우선 통과 처리
-        return true;
+        // 액세스 토큰의 유효성을 검증하고 사용자 ID를 반환
+        Long userId = tokenBusiness.validationAccessToken(accessToken);
+
+        if (userId != null) {
+            // 현재 요청의 속성을 가져옴. 요청이 없을 경우 null이 반환될 수 있으므로, null 체크를 수행하여 예외를 던짐.
+            RequestAttributes requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
+            // 인증된 사용자 ID를 현재 요청의 속성에 저장.
+            // "userId"라는 키로 userId 값을 요청 범위에 설정.
+            // 이 값은 요청이 종료되면 소멸됨.
+            requestContext.setAttribute("userId", userId, RequestAttributes.SCOPE_REQUEST);
+
+            // 인증 성공
+            return true;
+        }
+
+        // 인증 실패
+        throw new ApiException(ErrorCode.BAD_REQUEST, "인증 실패");
     }
 }
