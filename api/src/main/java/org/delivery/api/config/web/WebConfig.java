55bb0c56b7bfd2699ed929a8a28cb6db8fe35ec2
Seeeop2 <seeeop2@gmail.com>
2024-09-29 13:21:29 +0900

    FEAT: WebConfig 클래스에 UserSessionResolver 추가
    - WebConfig 클래스에 UserSessionResolver를 추가하여 사용자 세션 정보를 주입할 수 있도록 설정함.
    - addArgumentResolvers() 메서드를 구현하여 UserSessionResolver를 등록함.
    - 기존의 AuthorizationInterceptor 설정은 유지하고, 새로운 기능을 추가하여 코드의 기능성을 향상시킴.

--- api/src/main/java/org/delivery/api/config/web/WebConfig.java
+++ api/src/main/java/org/delivery/api/config/web/WebConfig.java
@@ -2,7 +2,9 @@ package org.delivery.api.config.web;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.interceptor.AuthorizationInterceptor;
+import org.delivery.api.resolver.UserSessionResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     // AuthorizationInterceptor 인스턴스 주입
     private final AuthorizationInterceptor authorizationInterceptor;
+    private final UserSessionResolver userSessionResolver;
 
     // 공개 API 경로 리스트
     private List<String> OPEN_API = List.of(
@@ -46,4 +49,11 @@ public class WebConfig implements WebMvcConfigurer {
                 // Swagger 관련 경로 제외
                 .excludePathPatterns(SWAGGER);
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        // UserSessionResolver를 추가하여 사용자 세션 정보를 주입할 수 있도록 설정
+        resolvers.add(userSessionResolver);
+    }
+
 }
