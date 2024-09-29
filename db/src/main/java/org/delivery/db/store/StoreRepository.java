6f0a5dd23982d203e154510bb67059a7de748b5b
Seeeop2 <seeeop2@gmail.com>
2024-09-30 00:05:46 +0900

    FEAT: StoreRepository 인터페이스 추가
    - StoreRepository 인터페이스 추가: StoreEntity에 대한 CRUD 작업을 위한 JPA 리포지토리 구현.
    - 유효한 스토어 조회를 위한 메서드 정의:
      - findFirstByIdAndStatusOrderByIdDesc: 특정 ID와 상태에 해당하는 첫 번째 스토어를 조회.
      - findAllByStatusOrderByIdDesc: 특정 상태의 모든 스토어를 조회하여 ID 기준으로 정렬.
      - findAllByStatusAndCategoryOrderByStarDesc: 특정 상태와 카테고리에 해당하는 스토어를 평점 기준으로 정렬하여 조회.

---
+++ db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -0,0 +1,24 @@
+package org.delivery.db.store;
+
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+    // 유효한 스토어
+    // SELECT * FROM STORE WHERE ID = ? AND STATUS = ? ORDER BY ID DESC LIMIT 1
+    Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
+
+    // 유효한 스토어 리스트
+    // SELECT * FROM STORE WHERE STATUS = ? ORDER BY ID DESC
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+
+    // 유효한 특정 카테고리의 스토어 리스트
+    // SELECT * FROM STORE WHERE STATUS = ? AND CATEGORY = ? ORDER BY STAR DESC
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
+
+}
