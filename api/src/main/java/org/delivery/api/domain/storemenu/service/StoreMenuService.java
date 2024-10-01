fae9faa431f9815ff3d1b3996060fc942392fd75
Seeeop2 <seeeop2@gmail.com>
2024-10-02 01:40:28 +0900

    FEAT: StoreMenuService 클래스 추가
    - 스토어 메뉴 관련 비즈니스 로직을 처리하는 서비스 클래스.
    - 메서드:
      - getStoreMenuWithThrow:
        - 특정 ID의 등록된 메뉴를 조회하고, 메뉴가 없을 경우 ApiException 발생.
      - getStoreMenuByStoreId:
        - 특정 가게 ID에 해당하는 모든 등록된 메뉴를 조회.
      - register:
        - 스토어 메뉴를 등록하고, 메뉴가 null일 경우 ApiException 발생.
        - 메뉴의 상태를 설정한 후 저장.

---
+++ api/src/main/java/org/delivery/api/domain/storemenu/service/StoreMenuService.java
@@ -0,0 +1,45 @@
+package org.delivery.api.domain.storemenu.service;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.db.storemenu.StoreMenuEntity;
+import org.delivery.db.storemenu.StoreMenuRepository;
+import org.delivery.db.storemenu.enums.StoreMenuStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class StoreMenuService {
+
+    private final StoreMenuRepository storeMenuRepository;
+
+    // 특정 ID의 등록된 메뉴를 조회하고, 없을 경우 예외 발생
+    public StoreMenuEntity getStoreMenuWithThrow(Long id){
+        Optional<StoreMenuEntity> entity = storeMenuRepository.findFirstByIdAndStatusOrderByIdDesc(id, StoreMenuStatus.REGISTERED);
+
+        // 메뉴가 없으면 ApiException 발생
+        return entity.orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    // 특정 가게의 등록된 모든 메뉴를 조회
+    public List<StoreMenuEntity> getStoreMenuByStoreId(Long storeId){
+        return storeMenuRepository.findAllByStoreIdAndStatusOrderBySequenceDesc(storeId, StoreMenuStatus.REGISTERED);
+    }
+
+    // 스토어 메뉴 등록
+    public StoreMenuEntity register(StoreMenuEntity storeMenuEntity){
+        return Optional.ofNullable(storeMenuEntity)
+                .map(it -> {
+                    // 메뉴의 상태 설정
+                    it.setStatus(storeMenuEntity.getStatus());
+                    // 메뉴 저장
+                    return storeMenuRepository.save(it);
+                })
+                // 메뉴가 null일 경우 ApiException 발생
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+}
