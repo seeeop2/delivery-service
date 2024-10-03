cc7bc5c76fab434831d291278b8ef0328b060c6c
Seeeop2 <seeeop2@gmail.com>
2024-10-03 23:20:47 +0900

    FEAT: UserOrderEntity 및 UserOrderStatus 추가
    - UserOrderEntity 클래스:
      - 사용자 주문 정보를 담는 JPA 엔티티.
      - 사용자 ID, 주문 상태, 주문 금액, 주문 및 처리 시간 관련 필드 포함.
      - Lombok을 사용하여 기본 생성자, 모든 필드를 포함한 생성자, Getter/Setter 자동 생성.
    
    - UserOrderStatus 열거형:
      - 사용자 주문의 다양한 상태를 정의.
      - 각 상태에 대한 설명을 포함하여 명확한 상태 관리를 지원.

---
+++ db/src/main/java/org/delivery/db/userorder/UserOrderEntity.java
@@ -0,0 +1,52 @@
+package org.delivery.db.userorder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.delivery.db.BaseEntity;
+import org.delivery.db.userorder.enums.UserOrderStatus;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@Table(name = "user_order")
+@Entity
+public class UserOrderEntity extends BaseEntity {
+
+    // 사용자 ID (1:N 관계)
+    @Column(nullable = false)
+    private Long userId;
+
+    // 주문 상태 (열거형)
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50, nullable = false)
+    private UserOrderStatus status;
+
+    // 주문 금액
+    @Column(precision = 11, scale = 4, nullable = false)
+    private BigDecimal amount;
+
+    // 주문 시간
+    private LocalDateTime orderAt;
+
+    // 주문 확인 시간
+    private LocalDateTime acceptedAt;
+
+    // 요리 시작 시간
+    private LocalDateTime cookingStartedAt;
+
+    // 배달 시작 시간
+    private LocalDateTime deliveryStartedAt;
+
+    // 주문 완료 시간
+    private LocalDateTime receivedAt;
+
+}
