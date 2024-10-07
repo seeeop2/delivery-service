348746d7e4b7242a7dbf2ac58807fd977444360f
Seeeop2 <seeeop2@gmail.com>
2024-10-07 22:57:02 +0900

    FEAT: UserOrderMenuService에 주문 생성 기능 추가
    - UserOrderMenuService 클래스:
      - 기존의 주문 메뉴 조회 기능 유지.
      - 새로운 주문을 등록하는 `order` 메서드 추가:
        - 주문 메뉴 엔티티를 받아 상태를 "등록"으로 설정한 후 저장.
        - null인 경우 ApiException을 발생시켜 에러 처리.

--- api/src/main/java/org/delivery/api/domain/userordermenu/service/UserOrderMenuService.java
+++ api/src/main/java/org/delivery/api/domain/userordermenu/service/UserOrderMenuService.java
@@ -1,12 +1,15 @@
 package org.delivery.api.domain.userordermenu.service;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.userordermenu.UserOrderMenuEntity;
 import org.delivery.db.userordermenu.UserOrderMenuRepository;
 import org.delivery.db.userordermenu.enums.UserOrderMenuStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 // 사용자 주문 메뉴 관련 서비스 클래스
 
@@ -23,4 +26,17 @@ public class UserOrderMenuService {
         return userOrderMenuRepository.findAllByUserOrderIdAndStatus(userOrderId, UserOrderMenuStatus.REGISTERED);
     }
 
+    // 새로운 주문 메뉴를 등록하는 메서드
+    public UserOrderMenuEntity order(UserOrderMenuEntity userOrderMenuEntity){
+        // 주문 메뉴 엔티티가 null인지 확인
+        return Optional.ofNullable(userOrderMenuEntity)
+                .map(it -> {
+                    // 상태를 등록으로 설정
+                    it.setStatus(UserOrderMenuStatus.REGISTERED);
+                    // 주문 메뉴를 저장하고 반환
+                    return userOrderMenuRepository.save(it);
+                })
+                // null일 경우 예외 발생
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
 }
