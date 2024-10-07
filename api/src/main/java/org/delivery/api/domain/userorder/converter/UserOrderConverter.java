aa97e1afa216c6dc45ce3f1d314078406da7bde8
Seeeop2 <seeeop2@gmail.com>
2024-10-08 01:50:09 +0900

    FEAT: UserOrderConverter 클래스 추가 및 변환 메서드 구현
    - UserOrderConverter 클래스:
      - 사용자와 스토어 메뉴 리스트를 기반으로 주문 엔티티를 생성하는 변환기 클래스.
    
    - toEntity 메서드:
      - 사용자와 스토어 메뉴 리스트를 입력받아 UserOrderEntity를 생성.
      - 스토어 메뉴 리스트의 금액을 합산하여 총 금액을 계산하고 설정.
    
    - toResponse 메서드:
      - UserOrderEntity를 UserOrderResponse로 변환하는 메서드.
      - 주문 ID, 상태, 금액, 주문 및 처리 시간 관련 정보를 설정하여 응답 객체를 생성.

---
+++ api/src/main/java/org/delivery/api/domain/userorder/converter/UserOrderConverter.java
@@ -0,0 +1,47 @@
+package org.delivery.api.domain.userorder.converter;
+
+import org.delivery.api.common.annotation.Converter;
+import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
+import org.delivery.db.storemenu.StoreMenuEntity;
+import org.delivery.db.userorder.UserOrderEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Converter
+public class UserOrderConverter {
+
+    // 사용자와 스토어 메뉴 리스트를 기반으로 UserOrderEntity를 생성하는 메서드
+    public UserOrderEntity toEntity(User user, List<StoreMenuEntity> storeMenuEntityList){
+
+        // 스토어 메뉴 리스트의 금액을 합산하여 총 금액 계산
+        BigDecimal totalAmount = storeMenuEntityList.stream()
+                // 각 스토어 메뉴의 금액을 추출
+                .map(it -> it.getAmount())
+                // 금액을 합산
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        // UserOrderEntity 빌더를 사용하여 객체 생성
+        return UserOrderEntity.builder()
+                .userId(user.getId())
+                // 총 금액 설정
+                .amount(totalAmount)
+                .build();
+    }
+
+    // UserOrderEntity를 UserOrderResponse로 변환하는 메서드
+    public UserOrderResponse toResponse(UserOrderEntity userOrderEntity){
+        return UserOrderResponse.builder()
+                .id(userOrderEntity.getId())
+                .status(userOrderEntity.getStatus())
+                .amount(userOrderEntity.getAmount())
+                .orderAt(userOrderEntity.getOrderAt())
+                .acceptedAt(userOrderEntity.getAcceptedAt())
+                .cookingStartedAt(userOrderEntity.getCookingStartedAt())
+                .deliveryStartedAt(userOrderEntity.getDeliveryStartedAt())
+                .receivedAt(userOrderEntity.getReceivedAt())
+                .build();
+    }
+
+}
