2e5ad51009a594bef19b93d77381d89222fc5cd3
Seeeop2 <seeeop2@gmail.com>
2024-10-11 22:58:52 +0900

    FEAT: UserOrderApiController에 현재 및 과거 주문 조회 기능 추가
    - UserOrderApiController 클래스:
      - 사용자 주문 관련 API 엔드포인트를 정의하는 컨트롤러.
    
    - 변경 사항:
      - 현재 진행 중인 주문 조회 기능 추가:
        - GET /api/user-order/current 엔드포인트 추가.
      - 과거 주문 내역 조회 기능 추가:
        - GET /api/user-order/history 엔드포인트 추가.
      - 특정 주문 1건에 대한 내역 조회 기능 추가:
        - GET /api/user-order/id/{orderId} 엔드포인트 추가.

--- api/src/main/java/org/delivery/api/domain/userorder/controller/UserOrderApiController.java
+++ api/src/main/java/org/delivery/api/domain/userorder/controller/UserOrderApiController.java
@@ -6,14 +6,13 @@ import org.delivery.api.common.annotation.UserSession;
 import org.delivery.api.common.api.Api;
 import org.delivery.api.domain.user.model.User;
 import org.delivery.api.domain.userorder.business.UserOrderBusiness;
+import org.delivery.api.domain.userorder.controller.model.UserOrderDetailResponse;
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user-order")
@@ -29,6 +28,33 @@ public class UserOrderApiController {
         UserOrderResponse response = userOrderBusiness.userOrder(user, userOrderRequest.getBody());
 
         // 성공적인 응답 반환
+        return Api.OK(response);
+    }
+
+    // 현재 진행 중인 주문 건 조회
+    @GetMapping("/current")
+    public Api<List<UserOrderDetailResponse>> current(@Parameter(hidden = true) @UserSession User user){
+
+        // 현재 주문 내역 조회
+        List<UserOrderDetailResponse> response = userOrderBusiness.current(user);
+        return Api.OK(response);
+    }
+
+    // 과거 주문 내역
+    @GetMapping("/history")
+    public Api<List<UserOrderDetailResponse>> history(@Parameter(hidden = true) @UserSession User user){
+
+        // 과거 주문 내역 조회
+        List<UserOrderDetailResponse> response = userOrderBusiness.history(user);
+        return Api.OK(response);
+    }
+
+    // 특정 주문 1건에 대한 내역 조회
+    @GetMapping("/id/{orderId}")
+    public Api<UserOrderDetailResponse> read(@Parameter(hidden = true) @UserSession User user, @PathVariable Long orderId){
+
+        // 특정 주문 내역 조회
+        UserOrderDetailResponse response = userOrderBusiness.read(user, orderId);
         return Api.OK(response);
     }
 
