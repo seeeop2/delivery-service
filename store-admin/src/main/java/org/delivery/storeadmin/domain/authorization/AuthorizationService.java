705998582dc027fc907a4dc8eee266655182ce0a
Seeeop2 <seeeop2@gmail.com>
2024-10-18 20:39:42 +0900

    REFACTOR: AuthorizationService 클래스 리팩토링 및 UserSession으로 변경
    - AuthorizationService 클래스 리팩토링:
      - UserDetailsService 인터페이스 구현을 유지하면서 UserDetails 객체 생성 방식을 변경.
    
    - 변경 사항:
      - StoreRepository를 추가하여 사용자의 스토어 정보를 조회.
      - UserDetails 객체를 Spring Security의 User 대신 UserSession으로 변경하여 사용자 세션 정보를 포함.
      - UserSession 클래스의 빌더 패턴을 사용하여 사용자 정보 및 스토어 정보를 포함한 객체 생성.

--- store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
@@ -1,9 +1,12 @@
 package org.delivery.storeadmin.domain.authorization;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreStatus;
 import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
 import org.delivery.storeadmin.domain.user.service.StoreUserService;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +19,7 @@ import java.util.Optional;
 public class AuthorizationService implements UserDetailsService {
 
     private final StoreUserService storeUserService;
+    private final StoreRepository storeRepository;
 
     // 사용자 이름으로 UserDetails 를 로드하는 메서드
     @Override
@@ -24,16 +28,26 @@ public class AuthorizationService implements UserDetailsService {
         // 사용자 이름을 기반으로 등록된 사용자 엔티티 조회
         Optional<StoreUserEntity> storeUserEntity = storeUserService.getRegisterUser(username);
 
+        Optional<StoreEntity> storeEntity = storeRepository.findFirstByIdAndStatusOrderByIdDesc(storeUserEntity.get().getStoreId(), StoreStatus.REGISTERED);
+
         // 사용자 엔티티가 존재하면 UserDetails 객체 생성
         return storeUserEntity.map(it -> {
 
-            UserDetails user = User.builder()
-                    .username(it.getEmail())
-                    .password(it.getPassword())
-                    .roles(it.getRole().toString())
-                    .build();
-            return user;
+            UserSession userSession = UserSession.builder()
+                .userId(it.getId())
+                .email(it.getEmail())
+                .password(it.getPassword())
+                .status(it.getStatus())
+                .role(it.getRole())
+                .registeredAt(it.getRegisteredAt())
+                .lastLoginAt(it.getLastLoginAt())
+                .unRegisteredAt(it.getUnRegisteredAt())
 
+                .storeId(storeEntity.get().getId())
+                .storeName(storeEntity.get().getName())
+                .build();
+
+            return userSession;
         }).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 
